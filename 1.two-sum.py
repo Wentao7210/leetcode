4f9af28ae28442154878e92433592c33e9d3d385
Wentao7210 <wentaosun.sh@gmail.com>
2023-06-21 00:33:41 +0100

    Array - Easy

---
+++ 1.two-sum.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] + nums[j] == target and i != j:
+                    return [i, j]      
+# @lc code=end
+
