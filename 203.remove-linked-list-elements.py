f817bdd3d33bb5bb67309884dc21b196f732eb9d
Wentao7210 <wentaosun.sh@gmail.com>
2023-07-16 17:01:14 +0100

    linked list - easy

--- 203.remove-linked-list-elements.py
+++ 203.remove-linked-list-elements.py
@@ -25,3 +25,33 @@ class Solution:
 
 # @lc code=end
 head = [1,2,6,3,4,5,6]
+
+### chatgpt4 version of annotations explanation
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:       
+        # Create a dummy node that acts as a pre-head node of the input list
+        dummyhead = ListNode(next=head)
+
+        # Initialize the current node as dummyhead
+        current = dummyhead
+
+        # Traverse through the linked list until reaching the end (None)
+        while current.next: # While there's a next node in the list
+
+            # If the next node's value equals the target value
+            if current.next.val == val: 
+                # Skip the next node, effectively removing it from the list
+                # This works because Python's garbage collection will remove objects not referred to
+                current.next = current.next.next 
+            else:
+                # Move to the next node in the list
+                current = current.next
+
+        # Return the head of the updated list, note that we skip the dummyhead
+        return dummyhead.next
